dbbedb81e82f13513f03dac3d325b41e2f55f31b
Kevin Altman <kevin@itsthatguy.com>
2018-05-02 16:01:06 -0400

    connect to DB on heroku

--- apps/api/ormconfig.js
+++ apps/api/ormconfig.js
@@ -1,10 +1,5 @@
 const baseOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '',
-  database: 'steps_admin_test',
   synchronize: false,
   logging: false,
   migrationsTableName: 'migrations',
@@ -19,12 +14,22 @@ const baseOptions = {
 };
 
 const productionOptions = {
+  host: process.env.DATABASE_URL || 'localhost',
   entities: [".build/entity/**/*.js"],
   migrations: [".build/migration/**/*.js"],
   subscribers: [".build/subscriber/**/*.js"]
 };
 
+const developmentOptions = {
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '',
+  database: 'steps_admin_test',
+}
+
 module.exports = Object.assign({},
   baseOptions,
   process.env.NODE_ENV === 'production' && productionOptions,
+  process.env.NODE_ENV !== 'production' && developmentOptions,
 );
