3d90523573bca25ae6a739e96cbd6589796ed221
Kevin Altman <kevin@itsthatguy.com>
2018-05-08 18:29:58 -0400

    src resolution for storybook

--- apps/admin/.storybook/webpack.config.js
+++ apps/admin/.storybook/webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
+const SRC = path.resolve(__dirname, '..', 'src');
 
 module.exports = (a, b, defaultConfig) => {
   defaultConfig.resolve.extensions.push('.ts', '.tsx');
+  defaultConfig.resolve.modules.push(SRC);
 
   defaultConfig.module.rules.push(
     {
       test: /\.tsx?$/,
       use: ['babel-loader', 'awesome-typescript-loader'],
       exclude: ['node_modules', '__tests__'],
-      include: path.resolve(__dirname, '../src/'),
+      include: SRC,
     }
   );
 
