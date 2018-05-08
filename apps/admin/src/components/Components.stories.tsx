3047b3cdb179c501647537aceeae9f0b065c7888
Kevin Altman <kevin@itsthatguy.com>
2018-05-08 18:30:15 -0400

    add router decorator to component stories

--- apps/admin/src/components/Components.stories.tsx
+++ apps/admin/src/components/Components.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
@@ -9,7 +10,10 @@ import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
 import Badge from './Badge';
 
 const Components = storiesOf('Components', module)
-  .addDecorator(withKnobs);
+  .addDecorator(withKnobs)
+  .addDecorator(story => (
+    <BrowserRouter>{story()}</BrowserRouter>
+  ));
 
 Components
   .add('Badge', () => <Badge text={text('Text', 'hello')} /> )
