a2321c9f01652d07131e12dbc7d524b7fd404aaf
Kevin Altman <kevin@itsthatguy.com>
2018-05-16 17:59:41 -0400

    Admin: Staff panel

--- apps/admin/src/routes/Admin/Staff.tsx
+++ apps/admin/src/routes/Admin/Staff.tsx
@@ -4,22 +4,41 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import styled from 'styled-components';
 
+import Panel from 'atoms/Panel';
+import InputRow from 'components/Forms/InputRow';
+import Button from 'atoms/Button';
 
 interface Props {
-  className?: string;
 }
 
 class Staff extends React.Component<Props, {}> {
   render () {
     return (
-      <div className={this.props.className}>
-        Admin Staff
-      </div>
+      <BaseStaff>
+        <div className='header'>
+          <h1>Staff</h1>
+          <Button>Invite Staff</Button>
+        </div>
+        <label>Name & Email</label>
+        <Panel>
+          <p>STUFF HERE</p>
+          <Button>Edit</Button>
+        </Panel>
+      </BaseStaff>
     );
   }
 }
-const StyledStaff = styled(Staff)`
-display: flex;
+const BaseStaff = styled.div`
+  flex: 2;
+  margin-left: 100px;
+  margin-right: 100px;
+
+  .header {
+    display: flex;
+    font-size: 16px;
+    align-items: baseline;
+    justify-content: space-between;
+  }
 `;
 
 const mapStateToProps = (state, props) => ({
@@ -28,4 +47,4 @@ const mapStateToProps = (state, props) => ({
 const mapDispatchToProps = dispatch => ({
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(StyledStaff);
+export default connect(mapStateToProps, mapDispatchToProps)(Staff);
