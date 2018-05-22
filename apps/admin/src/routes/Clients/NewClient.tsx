8402d6284f2fc0044468e0feadce456e089d675e
Pearl <pearlbea@gmail.com>
2018-05-22 13:56:13 -0500

    Adds New Client page content

--- apps/admin/src/routes/Clients/NewClient.tsx
+++ apps/admin/src/routes/Clients/NewClient.tsx
@@ -1,13 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { Flex, Box } from 'grid-styled';
+import Button from 'atoms/Button';
+import LabeledInput from 'atoms/LabeledInput';
+import Panel from 'atoms/Panel';
 
-export default class NewClient extends React.Component {
+const Content = styled.div`
+  position: relative;
+  padding: 2em 2em 6em 2em;
+  height: 100%;
+
+  button {
+    position: absolute;
+    bottom:0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+`;
+
+class NewClient extends React.Component {
   render () {
     return (
-      <div>
-        <h2>New Client</h2>
+      <div className='new-client'>
+        <Flex flexWrap='wrap'>
+          <Box width={[1, 1/2]} px={2}>
+            <Content>
+              <h2>Title</h2>
+              <Box>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Box>
+              <Button>Play Video</Button>
+            </Content>
+          </Box>
+          <Box width={[1, 1/2]} px={2}>
+            <Content>
+              <h2>Add New Client</h2>
+              <Flex flexWrap='wrap'>
+                <Box width={[1, 1/2]} px={2}><LabeledInput text='First' type='text' /></Box>
+                <Box width={[1, 1/2]} px={2}><LabeledInput text='Last' type='text' /></Box>
+                <Box w={1} px={2}><LabeledInput text='Email' type='email' /></Box>
+                <Box px={2}><LabeledInput text='Phone Number' type='tel' /></Box>
+              </Flex>
+              <Button>Save</Button>
+            </Content>
+          </Box>
+        </Flex>
         <Link to="/">Home</Link>
       </div>
     );
   }
 }
+
+export default NewClient;
