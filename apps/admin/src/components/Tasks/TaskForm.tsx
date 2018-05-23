2d960993e710ab97b090bba8d12464273bdf80c7
Pearl <pearlbea@gmail.com>
2018-05-23 15:38:19 -0500

    Add Add Step button

--- apps/admin/src/components/Tasks/TaskForm.tsx
+++ apps/admin/src/components/Tasks/TaskForm.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Flex, Box } from 'grid-styled';
 import styled from 'styled-components';
-import { green } from 'styles/colors';
+import { green, grey, white } from 'styles/colors';
 import Badge from 'atoms/Badge';
 import Button from 'atoms/Button'
+import { Link } from 'react-router-dom';
 import Panel from 'atoms/Panel';
 import StackedInputRow from 'components/Forms/StackedInputRow';
-import TaskStep from 'components/Tasks/TaskStep';
+
 
 interface Props {
   className?: string;
@@ -26,9 +27,13 @@ class TaskForm extends React.Component<Props, {}> {
           <form>
             <StackedInputRow type='text' label='Task'/>
             <StackedInputRow type='text' label='Why This Matters'/>
-            <Flex justifyContent='center'><Button>SAVE TO WORKPLAN</Button></Flex>
+
           </form>
           {children}
+
+          {/* TODO: What happens when this is clicked? */}
+          <div className='add-step-link'><Link to='/'>Add a step</Link></div>
+          <Flex justifyContent='center'><Button>SAVE TO WORKPLAN</Button></Flex>
         </Panel>
       </div>
     );
@@ -38,6 +43,32 @@ class TaskForm extends React.Component<Props, {}> {
 const StyledTaskForm = styled(TaskForm)`
   .task-badge {
     margin-bottom: 1em;
+  }
+  .add-step-link {
+    display: table;
+    margin: 1em 0;
+    text-align: center;
+    text-transform: uppercase;
+    white-space: nowrap;
+
+    &:before, &:after {
+      border-top: 1px solid ${grey};
+      content: '';
+      display: table-cell;
+      position: relative;
+      top: 0.5em;
+      width: 45%;
+    }
+    &:before {
+      right: 1.5%;
+    }
+    &:after {
+      left: 1.5%;
+    }
+    a {
+      color: ${grey};
+      text-decoration: none;
+    }
   }
 `;
 
