11a4f408a099495cc252e4964dd53113795bf7b7
Pearl <pearlbea@gmail.com>
2018-05-30 12:04:47 -0500

    Add no chat icons

--- apps/admin/src/components/Chat/NoChat.tsx
+++ apps/admin/src/components/Chat/NoChat.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Box } from 'grid-styled';
 import styled from 'styled-components';
+import { blue } from 'styles/colors';
 import Panel from 'atoms/Panel';
 
 interface Props {
@@ -13,18 +14,21 @@ class NoChat extends React.Component<Props, {}> {
   render() {
     const { className, type} = this.props;
     let message: string = '';
+    let icon: string = '';
 
     if(type === 'log') {
       message = "Chatting hasn't started. Come back later."
+      icon = 'message';
     } else {
       message = "Looks like your client hasn't messaged you yet."
+      icon = 'live_help';
     }
 
     return (
 
       <Box width={1} p={4} className={className}>
         <Panel>
-          <div>[Image Here]</div>
+          <div><i className='material-icons'>{icon}</i></div>
           <div>{message}</div>
         </Panel>
       </Box>
@@ -37,6 +41,10 @@ const StyledNoChat = styled(NoChat)`
   text-align: center;
   font-size: 1.5em;
   line-height: 1.5;
+  i {
+    font-size: 6em;
+    color: ${blue};
+  }
 `;
 
 export default StyledNoChat;
