a89e10a9a802f6825a1de5634ab6224a7333e7fb
Connor Mendenhall <ecmendenhall@gmail.com>
2018-06-02 00:07:52 -0400

    Refactor ChatMessage

--- apps/admin/src/atoms/ChatMessage.tsx
+++ apps/admin/src/atoms/ChatMessage.tsx
@@ -4,12 +4,10 @@ import styled from 'styled-components';
 import Panel from './Panel';
 import { white, mediumBlue } from 'styles/colors';
 
-export enum ChatMessageType {
-  sent = 'sent',
-  received = 'received'
-}
+type ChatMessageType = 'sent' | 'received';
 
 interface Props {
+  className?: string;
   type?: ChatMessageType;
   text: string;
   from?: string;
@@ -22,52 +20,49 @@ class ChatMessage extends React.Component<Props, {}> {
     color: mediumBlue
   };
 
-  renderChatElements() {
-    const { type, text, from } = this.props;
-    const chatElements = [
-      <Message {...this.props} key={1}>
-        {text}
-      </Message>,
-      <From {...this.props} key={2}>
-        {from}
-      </From>
-    ];
-    if (type === 'received') {
-      return chatElements.reverse();
-    }
-    return chatElements;
-  }
   render() {
-    return <Container>{this.renderChatElements()}</Container>;
+    const { className, type, text, from } = this.props;
+    return (
+      <div className={className}>
+        <Panel className="message">{text}</Panel>
+        <div className="from">{from}</div>
+      </div>
+    );
   }
 }
 
-const Container = styled.div`
-  font-family: 'Tiempos', serif;
-  font-size: 21px;
+const StyledChatMessage = styled<Props>(ChatMessage)`
+    font-family: 'Tiempos', serif;
+    font-size: 21px;
+    display: flex;
+    justify-content: flex-end;
+
+    flex-direction: ${({ type }) =>
+      type === 'received' ? 'row-reverse' : 'row'}
+
+    .message {
+      background-color: ${({ type, color }) =>
+        type === 'received' ? color : white};
+      display: inline-block;
+      padding: 20px;
+      line-height: 25px;
+    }
+
+    .from {
+      display: inline-block;
+      font-size: 28px;
+      width: 65px;
+      height: 65px;
+      border-radius: 65px;
+      color: ${white};
+      background-color: ${({ type, color }) =>
+        type === 'received' ? color : mediumBlue};
+      text-align: center;
+      vertical-align: middle;
+      line-height: 65px;
+      margin: 0 10px;
+    }
+  }
 `;
 
-export const Message = styled(Panel).attrs<Props>({})`
-  background-color: ${({ type, color }) =>
-    type === 'received' ? color : white};
-  display: inline-block;
-  padding: 20px;
-  line-height: 25px;
-`;
-
-export const From = styled<Props, 'div'>('div')`
-  display: inline-block;
-  font-size: 28px;
-  width: 65px;
-  height: 65px;
-  border-radius: 65px;
-  color: ${white};
-  background-color: ${({ type, color }) =>
-    type === 'received' ? color : mediumBlue};
-  text-align: center;
-  vertical-align: middle;
-  line-height: 65px;
-  margin: 0 10px;
-`;
-
-export default ChatMessage;
+export default StyledChatMessage;
--- apps/admin/src/atoms/__tests__/ChatMessage.test.tsx
+++ apps/admin/src/atoms/__tests__/ChatMessage.test.tsx
@@ -1,5 +1,5 @@
 import { shallow } from 'enzyme';
-import ChatMessage, { ChatMessageType, Message, From } from '../ChatMessage';
+import ChatMessage from '../ChatMessage';
 import * as React from 'react';
 
 describe('ChatMessage.tsx', () => {
@@ -7,27 +7,5 @@ describe('ChatMessage.tsx', () => {
     const wrapper = shallow(<ChatMessage text="test message" />);
 
     expect(wrapper).toMatchSnapshot();
-  });
-
-  it('renders message first if type is sent', () => {
-    const wrapper = shallow(
-      <ChatMessage type={ChatMessageType.sent} text="sent message" />
-    );
-    const message = wrapper.find(Message);
-    const from = wrapper.find(From);
-
-    expect(wrapper.childAt(0)).toEqual(message);
-    expect(wrapper.childAt(1)).toEqual(from);
-  });
-
-  it('renders from first if type is received', () => {
-    const wrapper = shallow(
-      <ChatMessage type={ChatMessageType.received} text="sent message" />
-    );
-    const message = wrapper.find(Message);
-    const from = wrapper.find(From);
-
-    expect(wrapper.childAt(0)).toEqual(from);
-    expect(wrapper.childAt(1)).toEqual(message);
   });
 });
