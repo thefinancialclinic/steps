14b2cecc3d0dd57c1558369e663c76c82b956f4d
Kevin Altman <kevin@itsthatguy.com>
2018-06-04 11:04:24 -0400

    stubs clients/tasks/orgs

--- apps/admin/src/reducers/clients.ts
+++ apps/admin/src/reducers/clients.ts
@@ -1,15 +1,83 @@
 import { SET_CLIENTS } from 'actions/clients';
 
+export type UserPlatform = 'SMS' | 'FBOOK';
+export type UserStatus = 'AWAITING_HELP' | 'WORKING' | 'NON_RESPONSIVE';
+
+type Client = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  coach_id: number;
+  org_id: number;
+  color: string;
+  goals: string[];
+  status: UserStatus;
+  updated: string;
+  platform: UserPlatform;
+  image: string;
+  follow_up_date: string;
+  checkin_times: CheckinTime[];
+  topic: string;
+};
+
+type CheckinTime = {
+  topic: string;
+  message: string;
+  time: string;
+};
+
+type Org = {
+  name: string;
+  sms_number: string;
+  logo: string;
+};
+
+interface State {
+  clients: Client[];
+  orgs: Org[];
+}
+
 const initialState = {
-  clients: [],
+  clients: [
+    {
+      first_name: 'string',
+      last_name: 'string',
+      email: 'user@example.com',
+      phone: 'string',
+      coach_id: 0,
+      org_id: 0,
+      color: 'string',
+      goals: ['string'],
+      status: 'AWAITING_HELP',
+      updated: '2018-06-04T14:44:20.085Z',
+      platform: 'SMS',
+      image: 'string',
+      follow_up_date: '2018-06-04T14:44:20.085Z',
+      checkin_times: [
+        {
+          topic: 'string',
+          message: 'string',
+          time: '2018-06-04T14:44:20.085Z'
+        }
+      ],
+      topic: 'string'
+    }
+  ],
+  orgs: [
+    {
+      name: 'string',
+      sms_number: 'string',
+      logo: 'string'
+    }
+  ]
 };
 
 export default (state = initialState, action) => {
-
   if (action.type === SET_CLIENTS) {
     return {
       ...state,
-      clients: action.clients,
+      clients: action.clients
     };
   }
 
