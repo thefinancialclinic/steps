06fecca5486093a8db4323769f4c2341ccffc93c
Kevin Altman <kevin@itsthatguy.com>
2018-06-04 14:22:16 -0400

    fix key dom issue

--- apps/admin/src/components/Tasks/TaskList.tsx
+++ apps/admin/src/components/Tasks/TaskList.tsx
@@ -45,7 +45,7 @@ const SortableList = SortableContainer(({ items }) => {
   return (
     <Box>
       {items.map((task, index) => (
-        <TaskContainer>
+        <TaskContainer key={index}>
           <div className='number'>{index + 1}</div>
           <Task key={`item-${index}`} index={index} value={task.title} id={task.id} userId={task.user_id} />
         </TaskContainer>
