b6bc7e744300df04fc58f87172d52735a267f01b
Becca Nelson <rnelson@8thlight.com>
2018-06-04 16:33:25 -0500

    Add Alert test with mock store

--- apps/admin/src/actions/alerts.ts
+++ apps/admin/src/actions/alerts.ts
@@ -1,15 +1,17 @@
+export const ADD_ALERT = 'ADD_ALERT';
 export const addAlert = (message, level, id) => {
   return {
-    type: 'ADD_ALERT',
+    type: ADD_ALERT,
     message,
     level,
     id
   };
 };
 
+export const REMOVE_ALERT = 'REMOVE_ALERT';
 export const removeAlert = id => {
   return {
-    type: 'REMOVE_ALERT',
+    type: REMOVE_ALERT,
     id
   };
 };
