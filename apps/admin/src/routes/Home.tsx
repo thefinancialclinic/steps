625071a8bef2f10d75c07a6ae57b0ae4f3786fe4
Kevin Altman <kevin@itsthatguy.com>
2018-06-06 13:04:56 -0400

    adds svg-inline-loader and svg background-image helper

--- apps/admin/src/routes/Home.tsx
+++ apps/admin/src/routes/Home.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 import Panel from 'atoms/Panel';
 import Button from 'atoms/Button';
+import { svgBackgroundImage } from 'styles/';
 
 interface Props {
   className?: string;
@@ -48,11 +49,31 @@ const StyledHome = styled(Home)`
 
     .blep {
       padding: 30px;
+      position: relative;
       text-align: center;
       width: 100%;
 
-      &:hover {
-        background-color: green;
+      > * {
+        justify-content: center;
+      }
+
+      &:before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+        opacity: 0;
+        transition: opacity 0.8s;
+      }
+
+      &:hover::before {
+        ${svgBackgroundImage('hover-bg.svg')}
+        background-position: center;
+        background-size: cover;
+        opacity: 1;
       }
     }
   }
