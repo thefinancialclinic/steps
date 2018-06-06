f45ee66b4efdacab6c94b80fdc149d2819b1f946
Kevin Altman <kevin@itsthatguy.com>
2018-06-06 10:43:55 -0400

    removes redux-storage

--- apps/admin/src/store.ts
+++ apps/admin/src/store.ts
@@ -6,18 +6,11 @@ import {
 } from 'redux';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise';
-import * as storage from 'redux-storage';
-import createEngine from 'redux-storage-engine-localstorage';
 
 import rootReducer from './reducers/index';
 
-const engine = createEngine('steps');
-
-const reducer = storage.reducer(rootReducer);
-const storageMiddleware = storage.createMiddleware(engine);
-
 const composeFn = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = [thunk, promise, storageMiddleware];
+const middlewares = [thunk, promise];
 const enhancer = composeFn(applyMiddleware(...middlewares));
 declare var window;
 declare var module;
@@ -27,9 +20,6 @@ export default function configureStore () {
     rootReducer,
     enhancer
   );
-
-  const load = storage.createLoader(engine);
-  load(store);
 
   if (module.hot) {
     module.hot.accept('./reducers', () => {
