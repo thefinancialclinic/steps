10754842330e8cf4d15adbc423c9bae8d69e604f
Kevin Altman <kevin@itsthatguy.com>
2018-06-19 13:39:02 -0400

    fix typescript error

--- apps/admin/src/routes/Admin/index.tsx
+++ apps/admin/src/routes/Admin/index.tsx
@@ -1,37 +1,33 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import { RoutesProps } from '../index';
+import { RoutesElement } from '../index';
 import { composeUserLayout } from 'layouts';
 
 import AdminProfile from './Profile';
 import AdminOrganization from './Organization';
 import AdminStaff from './Staff';
 
-type Params = { id: number };
+const Admin: RoutesElement = () => {
+  const { user } = this.props;
+  if (!user) return null;
 
-class Admin extends React.Component<RoutesProps, {}> {
-  render() {
-    const { user } = this.props;
-    if (!user) return null;
+  const links = [
+    { text: 'My Profile', to: '/profile' },
+    { text: 'Organization Info', to: '/organization' },
+    { text: 'Staff', to: '/staff' },
+  ];
 
-    const links = [
-      { text: 'My Profile', to: '/profile' },
-      { text: 'Organization Info', to: '/organization' },
-      { text: 'Staff', to: '/staff' },
-    ];
+  const composeLayout = Component =>
+    composeUserLayout(Component, { links, user, role: user.type });
 
-    const composeLayout = Component =>
-      composeUserLayout(Component, { links, user, role: user.type });
-
-    return (
-      <Switch>
-        <Route path="/profile" render={composeLayout(AdminProfile)} />
-        <Route path="/organization" render={composeLayout(AdminOrganization)} />
-        <Route path="/staff" render={composeLayout(AdminStaff)} />
-        <Route render={() => <Redirect to="/profile" />} />
-      </Switch>
-    );
-  }
-}
+  return (
+    <Switch>
+      <Route path="/profile" render={composeLayout(AdminProfile)} />
+      <Route path="/organization" render={composeLayout(AdminOrganization)} />
+      <Route path="/staff" render={composeLayout(AdminStaff)} />
+      <Route render={() => <Redirect to="/profile" />} />
+    </Switch>
+  );
+};
 
 export default Admin;
