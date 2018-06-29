a57129c83b20b92c29086c14b71ab1a7b90d4262
Becca Nelson <rnelson@8thlight.com>
2018-06-29 09:52:48 -0500

    Fix User model so org is not nested in authenticated user

--- apps/admin/src/routes/index.tsx
+++ apps/admin/src/routes/index.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Redirect, withRouter, Switch } from 'react-router-dom';
 
 import { connect } from 'react-redux';
-import { User, USER_TYPE } from 'reducers/auth';
+import { User, USER_TYPE, Org } from 'reducers/auth';
 
 import Admin from './Admin/index';
 import Client from './Client/index';
@@ -12,19 +12,21 @@ import Superadmin from './Superadmin/index';
 import UserSwitcher from 'components/util/UserSwitcher';
 import AuthRoutes from './Auth/index';
 import Login from 'routes/Auth/Login';
-import { getAuthenticatedUser } from 'actions/auth';
+import { getAuthenticatedUser, getOrg } from 'actions/auth';
 import { bindActionCreators } from 'redux';
 
 interface Props {
   children?: any;
   user: null | User;
+  org?: Org;
   history: any;
-  actions: { getAuthenticatedUser: Function };
+  actions: { getAuthenticatedUser: Function; getOrg: Function };
   isAuthenticated: null | boolean;
 }
 
 export interface RoutesProps {
   children?: any;
+  org?: Org;
   user?: User;
   history: any;
 }
@@ -33,12 +35,18 @@ export type RoutesElement = (props: RoutesProps) => any;
 
 class Routes extends React.Component<Props, {}> {
   componentDidMount() {
+    const { actions, user } = this.props;
     process.env.AUTH0_ENABLED === 'true' &&
-      this.props.actions.getAuthenticatedUser();
+      actions.getAuthenticatedUser().then(() => {
+        if (user.org_id) {
+          actions.getOrg(user.org_id);
+          console.log(this.props.org);
+        }
+      });
   }
 
   render() {
-    const { history, user, isAuthenticated } = this.props;
+    const { history, user, isAuthenticated, org } = this.props;
     const { type } = user;
     let RoleRoutes: RoutesElement = DefaultRoutes;
 
@@ -54,8 +62,8 @@ class Routes extends React.Component<Props, {}> {
         <Wrapper>
           {process.env.NODE_ENV === 'development' && <UserSwitcher />}
           <Switch>
-            <AuthRoutes user={user} history={history}>
-              <RoleRoutes user={user} history={history} />
+            <AuthRoutes user={user} org={org} history={history}>
+              <RoleRoutes user={user} org={org} history={history} />
             </AuthRoutes>
           </Switch>
         </Wrapper>
@@ -75,11 +83,12 @@ const Wrapper = styled.div`
 
 const mapStateToProps = state => ({
   user: state.auth.user,
+  org: state.auth.org,
   isAuthenticated: state.auth.isAuthenticated,
 });
 
 const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators({ getAuthenticatedUser }, dispatch),
+  actions: bindActionCreators({ getAuthenticatedUser, getOrg }, dispatch),
 });
 
 export default withRouter(
