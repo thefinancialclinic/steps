309c14a6742fb545b5eb1e9fe83868c80850b57b
Kevin Altman <kevin@itsthatguy.com>
2018-07-09 14:10:22 -0400

    more context into database actions

---
+++ apps/api/src/poolLogger.ts
@@ -0,0 +1,35 @@
+import { Client } from 'pg';
+
+export default pool => {
+  const refresh = 60000;
+  const update = () => {
+    console.log(
+      `-- [Pool] Health Update (${refresh / 1000}s) ------------------`,
+    );
+    console.log();
+    console.log(' - .totalCount  ', pool.totalCount);
+    console.log(' - .idleCount   ', pool.idleCount);
+    console.log(' - .waitingCount', pool.waitingCount);
+    console.log('------------------------------------------------');
+  };
+  update();
+  setInterval(update, refresh);
+
+  pool.on('connect', (client: Client) => {
+    console.log('[Pool]:connect');
+  });
+
+  pool.on('acquire', (client: Client) => {
+    console.log('[Pool]:acquire');
+  });
+
+  pool.on('error', (err, client: Client) => {
+    console.error('-- [Pool]:error ------------------------------');
+    console.error('error', err);
+    console.error('----------------------------------------------');
+  });
+
+  pool.on('remove', (client: Client) => {
+    console.log('[Pool]:remove');
+  });
+};
