de14831e55cc1c9dac39102036c826b05b6bc167
Connor Mendenhall <ecmendenhall@gmail.com>
2018-07-18 13:39:48 -0400

    Set default query

--- apps/admin/src/components/Superadmin/GraphQL.tsx
+++ apps/admin/src/components/Superadmin/GraphQL.tsx
@@ -5,6 +5,35 @@ import api from 'api';
 import styled from 'styled-components';
 import 'graphiql/graphiql.css';
 
+const defaultQuery = `# Welcome to GraphiQL
+#
+# GraphiQL is an in-browser tool for writing, validating, and
+# testing GraphQL queries.
+#
+# Type queries into this side of the screen, and you will see intelligent
+# typeaheads aware of the current GraphQL type schema and live syntax and
+# validation errors highlighted within the text.
+#
+# GraphQL queries typically start with a "{" character. Lines that starts
+# with a # are ignored.
+#
+# An example GraphQL query might look like:
+#
+#     {
+#       field(arg: "value") {
+#         subField
+#       }
+#     }
+#
+# Keyboard shortcuts:
+#
+#       Run Query:  Ctrl-Enter (or press the play button above)
+#
+#   Auto Complete:  Ctrl-Space (or just start typing)
+
+{ allOrgs { nodes { id, name }}}
+`;
+
 export const graphQLFetcher = graphQLParams => {
   return api
     .post('/postgraphile/graphql', graphQLParams, {
@@ -18,7 +47,7 @@ export const graphQLFetcher = graphQLParams => {
 
 const GraphQL: React.SFC<{}> = () => (
   <GraphiQLContainer>
-    <GraphiQL fetcher={graphQLFetcher} />
+    <GraphiQL fetcher={graphQLFetcher} query={defaultQuery} />
   </GraphiQLContainer>
 );
 
